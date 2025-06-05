8e0076ffbd1619f257e1ef8810911d1f73f91ee2
CharlieCisko <charlie@gdaytour.net>
2025-06-05 18:34:29 +0900

    feat: theme change

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,10 +1,13 @@
+vim.g.sonokai_style = "shusia"
+vim.g.sonokai_better_performance = 1
+
 return {
-  { "sainnhe/sonokai" },
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "sonokai",
-    },
+    "sainnhe/sonokai",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("sonokai")
+    end,
   },
-  lazy = false,
 }
