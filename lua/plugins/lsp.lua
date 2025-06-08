c4f853cd8eed98ce1055a029914e641c8ee23d80
CharlieCisko <charlie@gdaytour.net>
2025-06-08 21:45:53 +0900

    feat: clang format

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -85,6 +85,30 @@ return {
         },
       })
 
+      require("lspconfig").clangd.setup({
+        cmd = {
+          "clangd",
+          "--background-index",
+          "--clang-tidy",
+          "--clang-tidy-checks=*",
+          "--fallback-style=Google",
+        },
+        filetypes = { "c", "cpp", "objc", "objcpp" },
+        root_dir = require("lspconfig").util.root_pattern("compile_commands.json", ".git"),
+        on_attach = function(client, bufnr)
+          -- ① 문서 포맷팅 기능 활성화
+          client.server_capabilities.documentFormattingProvider = true
+
+          -- ② 저장 시 자동 포맷팅
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ bufnr = bufnr, async = false })
+            end,
+          })
+        end,
+      })
+
       keyMapper("K", vim.lsp.buf.hover)
       keyMapper("gd", vim.lsp.buf.definition)
       keyMapper("<leader>ca", vim.lsp.buf.code_action)
